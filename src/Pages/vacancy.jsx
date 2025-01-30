cb6f49afbf6cd13578079a025387203f2a9b0c66
term25100 <156735777+term25100@users.noreply.github.com>
2025-01-30 16:26:03 +0300

    добавлен футер

--- src/Pages/vacancy.jsx
+++ src/Pages/vacancy.jsx
@@ -1,12 +1,14 @@
 import react, { Component } from "react";
 import { Search } from "./Vacancy/search";
 import { Vacations } from "./Vacancy/vacations";
+import { Footer } from "../footer"
 export class Vacancy extends Component{
     render(){
         return(
             <div>
                 <Search/>
                 <Vacations/>
+                <Footer/>
             </div>
         )
     }
