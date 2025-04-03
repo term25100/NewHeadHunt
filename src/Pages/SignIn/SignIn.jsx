ec4c02f3c85f1d2185d900abbd69996067889402
term25100 <156735777+term25100@users.noreply.github.com>
2025-04-03 11:15:38 +0300

    fix resizable login frame

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -6,21 +6,19 @@ export function SignIn(){
             <div className="main_frame">
                 <div className="login_frame">
                     <div className='login_wrapper'>
-                        <h1>LOGIN</h1>
-                        <label htmlFor="email_adress">EMAIL ADRESS</label>
-                        <input type="email" id="email_adress" name="email_adress" placeholder='YOUR EMAIL ADRESS'/>
-                        <br />
-                        <label htmlFor="password">PASSWORD</label>
-                        <input type='password' id='password' name='password' placeholder='ENTER VALID PASSWORD'/>
-                        <a className='login_button' href="#">LOGIN</a>
+                        <h1>Авторизация</h1>
+                        <label htmlFor="email_adress">EMAIL АДРЕС</label>
+                        <input type="email" id="email_adress" name="email_adress" placeholder='ВАШ EMAIL АДРЕС'/>
+                        <label htmlFor="password">ПАРОЛЬ</label>
+                        <input type='password' id='password' name='password' placeholder='ВВЕДИТЕ ВАШ ПАРОЛЬ'/>
+                        <a className='login_button' href="#">ВОЙТИ</a>
                     </div>
                     <a className='close' href="/"></a>
                 </div>
                 <div className="prereg_frame">
-                    <h1>NOT A<br /> MEMBER?</h1>
+                    <h1>ВЫ НЕ<br /> УЧАСТНИК?</h1>
                     <p>Если вы станете нашим участником то сможете писать свои отзывы о работе в компаниях а также размещать свои вакансии и резюме! <br /> Также вы сможете сохранять понравившиеся анкеты либо вакансии, зарегистрируйтесь.</p>
-
-                    <a className='prereg_button' href="#">REGISTER NOW</a>  
+                    <a className='prereg_button' href="#">ЗАРЕГИСТРИРОВАТЬСЯ</a>  
                 </div>
             </div>
         </div>
